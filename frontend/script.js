792b82c73210a97d03be4db7246238f425938be5
Ben Tran <windwalker46@users.noreply.github.com>
2026-02-01 17:59:15 -0600

    Display role list when Select Role is clicked

--- frontend/script.js
+++ frontend/script.js
@@ -1,8 +1,28 @@
-// load flashcards from JSON file and log to console
+let flashcardsData = [];
+
+// load flashcards from JSON file
 async function loadFlashcards() {
   const res = await fetch("flashcards.json");
   const data = await res.json();
-  console.log(data);
+  flashcardsData = data;
 }
 
-loadFlashcards();
+// Show roles on the page
+function showRoles() {
+  const roleList = document.getElementById("role_list");
+  roleList.innerHTML = "";
+
+  flashcardsData.forEach(roleObject => {
+    const roleItem = document.createElement("div");
+    roleItem.textContent = roleObject.role;
+    roleList.appendChild(roleItem);
+  });
+}
+
+// run when page has loaded
+document.addEventListener("DOMContentLoaded", () => {
+  loadFlashcards();
+  const selectRoleButton = document.querySelector(".role_button");
+  // Click handler to show roles
+  selectRoleButton.addEventListener("click", showRoles);
+});
